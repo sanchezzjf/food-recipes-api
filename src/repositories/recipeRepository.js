c23feebb7075462aa86da4f4eaf4e474c8841a9b
Joao Sanchez <joao.f13@hotmail.com>
2022-07-02 08:54:41 -0300

    created the recipes repositorie

--- src/repositories/recipeRepository.js
+++ src/repositories/recipeRepository.js
@@ -1,26 +1,40 @@
 import { readFile, writeFile } from 'node:fs/promises'
 
 export default class RecipeRepository {
-    constructor({ file }){
-        this.file = file
+    constructor({ database }){
+        this.database = database
     }
 
     //private function
     async #currentFileContent(){
-        return JSON.parse(await readFile(this.file))
+        return JSON.parse(await readFile(this.database))
     }
 
     find(){
         return this.#currentFileContent()
     }
 
-    create(data){
-        const currentFile = this.#currentFileContent()
+    async create(data){
+        const currentFile = await this.#currentFileContent()
         currentFile.push(data)
 
-        await writeFile(this.file, JSON.stringify(currentFile))
+        await writeFile(this.database, JSON.stringify(currentFile))
 
         return data.id
     }
-
 }
+/* 
+const recipeRepository = new RecipeRepository({ database: './database/db.json' })
+
+
+console.log(
+    await recipeRepository.create({
+        id: 2,
+        title: 'teste'
+    })
+)
+
+console.log(
+    await recipeRepository.find()
+)
+ */
